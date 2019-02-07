4071b6b89e6eb7477b7f0750acef60d98b791adb
Tim <tim.wenderholm1931@gmail.com>
2019-02-07 15:56:05 +0100

    Added NEW Scoreboard

---
+++ src/tim03we/scoreboard/Scoreboard.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace tim03we\scoreboard;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use tim03we\scoreboard\ScoreboardTask;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\Player;
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
+use pocketmine\utils\Config;
+
+class Scoreboard extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("Plugin enabled!");
+        $this->saveResource("settings.yml");
+        $settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+        if($settings->get("GroupSystem") == "GlobalGroups") {
+            if(!$this->getServer()->getPluginManager()->getPlugin("GlobalGroups")){
+                $this->getLogger()->critical("GlobalGroups does not exist!");
+                $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin('Scoreboard'));
+            }
+        } else if($settings->get("GroupSystem") == "PurePerms") {
+            if(!$this->getServer()->getPluginManager()->getPlugin("PurePerms")){
+                $this->getLogger()->critical("PurePerms does not exist!");
+                $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin('Scoreboard'));
+            }
+        } else if(!$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")){
+            $this->getLogger()->critical("EconomyAPI does not exist!");
+            $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin('Scoreboard'));
+        } else if($settings->get("Scoreboard Activate") == "false") {
+            $this->getLogger()->notice("The plugin was deactivated because it was disabled in the config!");
+            $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin('Scoreboard'));
+        }
+    }
+	
+    public function onJoin (PlayerJoinEvent $event){
+        $settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+        $this->getscheduler()->scheduleRepeatingTask(new ScoreboardTask($this), $settings->get("Update Task"));
+    }
+
+    public function setScoreboardEntry(Player $player, int $score, string $msg, string $objName) {
+        $entry = new ScorePacketEntry();
+        $entry->objectiveName = $objName;
+        $entry->type = 3;
+        $entry->customName = " $msg   ";
+        $entry->score = $score;
+        $entry->scoreboardId = $score;
+        $pk = new SetScorePacket();
+        $pk->type = 0;
+        $pk->entries[$score] = $entry;
+        $player->sendDataPacket($pk);
+    }
+
+    public function rmScoreboardEntry(Player $player, int $score) {
+        $pk = new SetScorePacket();
+        if(isset($pk->entries[$score])) {
+            unset($pk->entries[$score]);
+            $player->sendDataPacket($pk);
+        }
+    }
+
+    public function createScoreboard(Player $player, string $title, string $objName, string $slot = "sidebar", $order = 0) {
+        $pk = new SetDisplayObjectivePacket();
+        $pk->displaySlot = $slot;
+        $pk->objectiveName = $objName;
+        $pk->displayName = $title;
+        $pk->criteriaName = "dummy";
+        $pk->sortOrder = $order;
+        $player->sendDataPacket($pk);
+    }
+
+    public function rmScoreboard(Player $player, string $objName) {
+        $pk = new RemoveObjectivePacket();
+        $pk->objectiveName = $objName;
+        $player->sendDataPacket($pk);
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("Plugin disabled!");
+    }
+}
---
+++ src/tim03we/scoreboard/ScoreboardTask.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace tim03we\scoreboard;
+
+use pocketmine\scheduler\Task;
+use tim03we\scoreboard\Scoreboard;
+use iTzFreeHD\GG\GGAPI;
+use iTzFreeHD\GG\GlobalGroups;
+use iTzFreeHD\GG\Listeners;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class ScoreboardTask extends Task {
+	
+    public $plugin;
+    
+	public function __construct(Scoreboard $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	public function onRun(int $currentTick) : void {
+        $settings = new Config($this->plugin->getDataFolder() . "settings.yml", Config::YAML);
+        $pl = $this->plugin->getServer()->getOnlinePlayers();
+        foreach ($pl as $player) {
+            if($settings->get("GroupSystem") == "PurePerms") {
+                $pp = $this->plugin->getServer()->getPluginManager()->getPlugin("PurePerms");
+                $group = $pp->getUserDataMgr()->getGroup($player, null);
+            }
+            if($settings->get("GroupSystem") == "GlobalGroups") {
+                $ggapi = new GGAPI(GlobalGroups::getInstance());
+                $pinfo = $ggapi->getPlayerInfo($player);
+            }
+            $playercount = count($this->plugin->getServer()->getOnlinePlayers());
+			$money = $this->plugin->getServer()->getPluginManager()->getPlugin('EconomyAPI')->myMoney($player);
+			$registered = count(glob($this->plugin->getServer()->getDataPath(). "players/*.dat"));
+			$xpos = $player->getX();
+			$ypos = $player->getY();
+            $zpos = $player->getZ();
+            $name = $player->getName();
+            $ping = $player->getPing();
+            $maxpcount = Server::getInstance()->getMaxPlayers();
+            $tps = Server::getInstance()->getTicksPerSecond();
+
+            $this->plugin->rmScoreboard($player, "objektName");
+            $this->plugin->createScoreboard($player, $this->convert($settings->get("title"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 1, $this->convert($settings->get("line1"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 2, $this->convert($settings->get("line2"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 3, $this->convert($settings->get("line3"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 4, $this->convert($settings->get("line4"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 5, $this->convert($settings->get("line5"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 6, $this->convert($settings->get("line6"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 7, $this->convert($settings->get("line7"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 8, $this->convert($settings->get("line8"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 9, $this->convert($settings->get("line9"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 10, $this->convert($settings->get("line10"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 11, $this->convert($settings->get("line11"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 12, $this->convert($settings->get("line12"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 13, $this->convert($settings->get("line13"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 14, $this->convert($settings->get("line14"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+            $this->plugin->setScoreboardEntry($player, 15, $this->convert($settings->get("line15"), $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps), "objektName");
+        }
+    }
+
+    public function convert(string $string, int $money, $group, $xpos, $ypos, $zpos, $playercount, $maxpcount, $registered, $name, $ping, $tps): string{
+        $string = str_replace("{money}", $money, $string);
+        $string = str_replace("{group}", $group, $string);
+        $string = str_replace("{x}", $xpos, $string);
+        $string = str_replace("{y}", $ypos, $string);
+        $string = str_replace("{z}", $zpos, $string);
+        $string = str_replace("{online_players}", $playercount, $string);
+        $string = str_replace("{max_players}", $maxpcount, $string);
+        $string = str_replace("{registered}", $registered, $string);
+        $string = str_replace("{name}", $name, $string);
+        $string = str_replace("{ping}", $ping, $string);
+        $string = str_replace("{tps}", $tps, $string);
+        return $string;
+	}
+}
